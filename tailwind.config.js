423be9cb044eb3e76022cc0d3881d0cf8d63714c
ifeanyi okwuashaka <rectaplay@gmail.com>
2024-07-17 09:24:28 +0100

    html output for home design initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-yellow':'#BAA333',
+        'brand-green':'#009674',
+        'brand-pink':'#C7186F'
+      }
+    },
+    fontSize: {
+      xs:'0.6rem',
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
+  },
+  plugins: [],
+}
+
